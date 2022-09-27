a8014264fcfe6af288e97139d00acbc098f4089f
arnoldteo <arnold@infotectsecurity.com>
2022-09-27 21:53:33 +0800

    Added help message for BuyerMode

--- src/main/java/com/nerdyteo/show_booking/mode/buyer/BuyerMode.java
+++ src/main/java/com/nerdyteo/show_booking/mode/buyer/BuyerMode.java
@@ -13,7 +13,18 @@ public class BuyerMode extends Mode {
 
     @Override
     protected String getHelpMessage() {
-        return "";
+        return "[Buyer Mode]\n" +
+                "\tAvailable Commands:\n" +
+                "\t\t- View\n" +
+                "\t\t\t(To list all available shows)\n" +
+                "\t\t- Availability <Show Number>\n" +
+                "\t\t\t(To list all available seat numbers for a show)\n" +
+                "\t\t- Book <Show Number> <Phone#> <Comma separated list of seats>\n" +
+                "\t\t\t(To book a seat in a show. Ticket # will be displayed if successful)\n" +
+                "\t\t- Cancel <Ticket#>  <Phone#>\n" +
+                "\t\t\t(To cancel a ticket.)\n" +
+                "\t\t- Exit\n" +
+                "\t\t\t(To exit Admin Mode and return to Mode Selection Window)";
     }
 
     public static BuyerMode getInstance() {
