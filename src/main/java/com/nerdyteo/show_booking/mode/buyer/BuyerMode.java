c6ed17d5837cf72a51699b9dfa41d8c4eb275bd2
arnoldteo <arnold@infotectsecurity.com>
2022-09-27 09:11:55 +0800

    Added BuyerMode with no implementations added

---
+++ src/main/java/com/nerdyteo/show_booking/mode/buyer/BuyerMode.java
@@ -0,0 +1,30 @@
+package com.nerdyteo.show_booking.mode.buyer;
+
+import com.nerdyteo.show_booking.mode.Mode;
+
+public class BuyerMode extends Mode {
+
+    private static volatile BuyerMode instance;
+
+
+    @Override
+    protected boolean commands() {
+        return false;
+    }
+
+    @Override
+    protected void printHelp() {
+
+    }
+
+    public static BuyerMode getInstance() {
+        if (instance == null) {
+            synchronized (BuyerMode.class) {
+                if (instance == null) {
+                    instance = new BuyerMode();
+                }
+            }
+        }
+        return instance;
+    }
+}
