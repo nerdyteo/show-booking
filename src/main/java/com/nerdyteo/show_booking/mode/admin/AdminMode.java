e0d8cce37796c8ea26abdc6e14d76c9da539a871
arnoldteo <arnold@infotectsecurity.com>
2022-09-27 09:11:38 +0800

    Added AdminMode with no implementations added

---
+++ src/main/java/com/nerdyteo/show_booking/mode/admin/AdminMode.java
@@ -0,0 +1,33 @@
+package com.nerdyteo.show_booking.mode.admin;
+
+import com.nerdyteo.show_booking.mode.Mode;
+
+public class AdminMode extends Mode {
+
+    private static volatile AdminMode instance;
+
+    private AdminMode() {
+    }
+
+    @Override
+    protected boolean commands() {
+        return false;
+    }
+
+    @Override
+    protected void printHelp() {
+
+    }
+
+
+    public static AdminMode getInstance() {
+        if (instance == null) {
+            synchronized (AdminMode.class) {
+                if (instance == null) {
+                    instance = new AdminMode();
+                }
+            }
+        }
+        return instance;
+    }
+}
