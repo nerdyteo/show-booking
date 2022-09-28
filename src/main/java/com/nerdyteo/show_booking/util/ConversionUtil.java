c131cec186f45de593b13095d3322b7685bed91d
arnoldteo <arnold@infotectsecurity.com>
2022-09-28 09:27:11 +0800

    Added toList method to ConversionUtil to convert String to List

--- src/main/java/com/nerdyteo/show_booking/util/ConversionUtil.java
+++ src/main/java/com/nerdyteo/show_booking/util/ConversionUtil.java
@@ -1,5 +1,10 @@
 package com.nerdyteo.show_booking.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class ConversionUtil {
 
     public static Long toLong(final String value) {
@@ -16,5 +21,11 @@ public class ConversionUtil {
         } catch (Exception error) {
             return null;
         }
+    }
+
+    public static <T> List<T> toList(final String value, final String delimiter, final Function<String, T> convert) {
+        return Arrays.stream(value.split(delimiter))
+                .map(convert)
+                .collect(Collectors.toList());
     }
 }
